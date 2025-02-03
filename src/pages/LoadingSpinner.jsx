788dd0e070901274d120e7f4b280a0747e633f7a
kaviraj121 <kavir916@gmail.com>
2025-02-03 20:57:27 +0530

    added a text in Loadingspinner

--- src/pages/LoadingSpinner.jsx
+++ src/pages/LoadingSpinner.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
+      {/* Spinner */}
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+
+      {/* Text below the spinner */}
+      <p className="mt-4 text-gray-600 text-center">
+        Please wait, the application is launching...<br />
+        This may take some time.
+      </p>
     </div>
   );
 };
